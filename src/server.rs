2054cf3107b6b27f04a151a0800eabd10875b127
artemiy.bezukladnikov <abe@datacorp.ee>
2021-05-19 20:17:49 +0300

    format code

--- src/server.rs
+++ src/server.rs
@@ -3,7 +3,7 @@ mod model;
 use crate::model::{new_model, Transcript};
 use crate::stt_model::recognition_server::{Recognition, RecognitionServer};
 use crate::stt_model::{RecognitionRequest, RecognitionResponse};
-use deepspeech::Model;
+use deepspeech::{Model, Stream};
 use std::sync::{Arc, Mutex};
 use tonic::transport::Server;
 use tonic::{Request, Response, Status};
@@ -12,8 +12,9 @@ mod stt_model {
     include!("../proto/stalin.rs");
 }
 struct GrpcServer {
-    //deepspeech: Model,
+    // deepspeech: Arc<Mutex<Model>>,
 }
+
 
 #[tonic::async_trait]
 impl Recognition for GrpcServer {
@@ -22,9 +23,9 @@ impl Recognition for GrpcServer {
         request: Request<RecognitionRequest>,
     ) -> Result<Response<RecognitionResponse>, Status> {
         let snippet = request.into_inner().snippet;
-        let mut model = new_model().unwrap();
-        let text = model.transcript(snippet).unwrap();
-
+        //let mut model = new_model().unwrap();
+        // let text = model.transcript(snippet).unwrap();
+        let text = "123".to_string();
         // let response = stt_model::RecognitionResponse { text };
         let response = stt_model::RecognitionResponse { text };
         Ok(Response::new(response))
@@ -35,9 +36,9 @@ impl Recognition for GrpcServer {
 async fn main() -> Result<(), anyhow::Error> {
     simple_logger::SimpleLogger::new().init().unwrap();
     let server_address = std::env::var("SERVER_ADDRESS")?.parse()?;
-    // let model = new_model().unwrap();
-    let mut grpc_server = GrpcServer {
-        // deepspeech: Arc::new(Mutex::new(model)),
+    // let mut model = new_model().unwrap();
+    let grpc_server = GrpcServer {
+         // deepspeech: Arc::new(Mutex::new(model)),
     };
     Server::builder()
         .add_service(RecognitionServer::new(grpc_server))
